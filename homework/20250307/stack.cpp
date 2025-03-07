b3a94a95013954e1cca7d50cae258bbc0ce78bb8
Young <young20050727@gmail.com>
2025-03-07 10:34:57 +0800

    update stack

--- homework/20250307/stack.cpp
+++ homework/20250307/stack.cpp
@@ -1,28 +1,42 @@
 #include <array>
 #include <print>
-#include <stdexcept>
+#include <iostream>
 
 class stack {
 public:
     stack() = default;
 
+    void print() const {
+        std::print("[");
+        for (size_t i = 0; i < size; ++i) {
+            std::print("{}", data[i]);
+            if (i != size - 1) {
+                std::print(", ");
+            }
+        }
+        std::print("]\n");
+    }
+
     void push(int value) {
-        if (size == capacity) {
-            throw std::runtime_error("Stack is full");
+        if (is_full()) {
+            std::print("Stack is full\n");
+            return;
         }
         data[size++] = value;
     }
 
     int pop() {
-        if (size == 0) {
-            throw std::runtime_error("Stack is empty");
+        if (is_empty()) {
+            std::print("Stack is empty\n");
+            return -1;  // 代表取消操作
         }
         return data[--size];
     }
 
     int top_item() const {
-        if (size == 0) {
-            throw std::runtime_error("Stack is empty");
+        if (is_empty()) {
+            std::print("Stack is empty\n");
+            return -1;
         }
         return data[size - 1];
     }
@@ -37,16 +51,40 @@ private:
     std::array<int, capacity> data{};
 };
 
+int choice() {
+    int choice;
+    std::print("1. push\n");
+    std::print("2. pop\n");
+    std::print("3. exit\n");
+    std::print("choice: ");
+    std::cin >> choice;
+    return choice;
+}
+
 int main() {
     stack s;
-    s.push(1);
-    s.push(2);
-    s.push(3);
-    std::print("{}\n", s.top_item());
-    s.pop();
-    std::print("{}\n", s.top_item());
-    s.pop();
-    std::print("{}\n", s.top_item());
-    s.pop();
-    std::print("{}\n", s.is_empty());
+
+    while (true) {
+        std::print("\n");
+        s.print();
+        switch (choice()) {
+            case 1:
+                int value;
+                std::print("Enter value: ");
+                std::cin >> value;
+                s.push(value);
+                break;
+            case 2:
+                int popped;
+                popped = s.pop();
+                if (popped != -1) {  // 只有當 pop 成功時才印出數值
+                    std::print("{}\n", popped);
+                }
+                break;
+            case 3:
+                return 0;
+            default:
+                std::print("Invalid choice\n");
+        }
+    }
 }
