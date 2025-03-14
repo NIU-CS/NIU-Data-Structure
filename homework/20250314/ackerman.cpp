c70cc3a75a05d25f6c8deb943b8a59ca6dd104f2
Young <young20050727@gmail.com>
2025-03-14 11:46:32 +0800

    add ackerman.cpp

---
+++ homework/20250314/ackerman.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+
+int ackerman(int m, int n) {
+    if (m == 0) return n + 1;
+    if (n == 0) return ackerman(m - 1, 1);
+    return ackerman(m - 1, ackerman(m, n - 1));
+}
+
+int main() {
+    int ans = ackerman(3, 3);
+
+    std::cout << ans << std::endl;
+    return 0;
+}
